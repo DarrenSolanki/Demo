b94ba8e619ef4cfdd0642506e55dde18531e701e
Dharmender <dharmender.singh@aceturtle.com>
2019-04-19 16:56:55 +0530

    expression

---
+++ Test/src/test/Datatype.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		
+		double km =(100*1.609344);
+		int highscore = 50;
+		
+		if(highscore==50)
+		{
+			System.out.println("This is an expression");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
