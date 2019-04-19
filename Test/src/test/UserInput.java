3f13fdcbf8a41bb596fee16af9b5ec3845ec715e
Dharmender <dharmender.singh@aceturtle.com>
2019-04-19 12:01:41 +0530

    user input

---
+++ Test/src/test/UserInput.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.util.Scanner;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); 
+	    System.out.println("Enter number");
+	    int a = sc.nextInt();
+	    System.out.println("Enter another number");
+	    int b = sc.nextInt();
+	    
+	    int product= a*b;
+	    System.out.println("Product is: "+ product);
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
