457b6afb7be5653e8f94eae82ef705d2dd1daa70
Dharmender <dharmender.singh@aceturtle.com>
2019-04-22 17:42:40 +0530

    test

--- Test/src/test/Addition.java
+++ Test/src/test/Addition.java
@@ -4,10 +4,10 @@ public class Addition {
 
 	public static void main(String[] args) {
 		
-		int a= 10;
+		int c= 20;
 		int b= 20;
 		
-		int sum=a+b;
+		int sum=c+b;
 		System.out.println("sum is:" +sum);
 		// TODO Auto-generated method stub
 
