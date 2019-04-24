a820656307a32766493278393bcc17f27c8f53e4
Dharmender <dharm.09@gmail.com>
2019-04-24 19:04:33 +0530

    Maintuesday

--- Test/src/tuesday/Main.java
+++ Test/src/tuesday/Main.java
@@ -3,8 +3,32 @@ package tuesday;
 public class Main {
 
 	public static void main(String[] args) {
+		int newScore = calculateScore("Rahul", 1000);
+		System.out.println("NewScore is : "+ newScore);
 		// TODO Auto-generated method stub
+		int newscore = calculateScore(100);
+		System.out.println(newscore);
 
 	}
-
+	
+	public static int calculateScore(String playername, int score) {
+		
+		System.out.println("Player :"+playername+ " scored "+ score+ " points");
+		return score * 1000;
+	}
+	
+	public static int calculateScore(int score) {
+		System.out.println("Unnammed Player scored : "+ score + " points");
+		return score*1000;
+	}
+	
+	public static int calculateScore(int score,int score1, int score2) {
+		System.out.println("Unnammed Player scored : "+ score + " points");
+		return score*1000;
+	}
+	
+	public static int calculateScore(int score, int score2) {
+		System.out.println("Unnammed Player scored : "+ score + " points");
+		return score*1000;
+	}
 }
