7cd05c0afcd33b78a42e9ee286b381d0e3442012
DarrenSolanki <dharm.09@gmail.com>
2020-01-24 07:15:39 +0530

    increased i value

--- Test/src/javaCode/SampleSwitch.java
+++ Test/src/javaCode/SampleSwitch.java
@@ -4,7 +4,8 @@ public class SampleSwitch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=0; i<6; i++) {
+		
+		for(int i=0; i<8; i++) {
 			switch(i) {
 			case 0 : System.out.println("i is zero");
 					break;
