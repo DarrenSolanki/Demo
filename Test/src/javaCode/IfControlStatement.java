60a72900b6dc862a79c51b8e127d5933e5e503a8
DarrenSolanki <dharm.09@gmail.com>
2020-02-06 21:28:39 +0530

    if statement

---
+++ Test/src/javaCode/IfControlStatement.java
@@ -0,0 +1,28 @@
+package javaCode;
+
+import java.util.Scanner;
+
+public class IfControlStatement {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter vaue of A");
+		int a = sc.nextInt();
+		System.out.println("Enter value of B");
+		int b = sc.nextInt();
+		
+		if(a<b){
+			System.out.println("A is less than B");
+			}
+		
+		else if(a > b) {
+				System.out.println("A is greater than B");
+			}
+		else if(a == b) {
+			System.out.println("A is equal to B");
+		}
+			
+
+	}
+
+}
